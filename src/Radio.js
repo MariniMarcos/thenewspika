778620b947ca44e7a4c37301f143e54876e8dd7b
Marini Marcos <mmarini95@hotmail.com>
2023-11-13 13:08:21 -0300

    Arreglo centrado boton play

--- src/Radio.js
+++ src/Radio.js
@@ -33,7 +33,7 @@ export default function Radio({ nombre, frecuencia, url }) {
     >
       <CardBody>
         <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-          <div className="flex flex-col col-span-6 md:col-span-8">
+          <div className="flex flex-col col-span-6 md:col-span-8 tarjeta">
           <div className="relative col-span-6 md:col-span-4 foto">
             <Image
               alt="Album cover"
@@ -50,7 +50,7 @@ export default function Radio({ nombre, frecuencia, url }) {
                 <h2 className="font-semibold text-foreground/90">{`${frecuencia}`}</h2>
               </div>
             </div>
-            <div className="flex w-full items-center justify-center">
+            <div className="flex w-full items-center justify-end">
               <Button
                 isIconOnly
                 className="w-auto h-auto data-[hover]:bg-foreground/10"
